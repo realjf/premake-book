cd9e7c4afbcedff9d599ee829e207269c3bf65e5
realjf <real_jf@hotmail.com>
2021-05-15 13:44:06 +0800

    update

--- push-to-github.bat
+++ push-to-github.bat
@@ -9,7 +9,7 @@ git push origin master
 
 git checkout gh-pages
 git checkout master
-git subtree pull --prefix=_book origin gh-pages --squash
+git subtree pull --prefix=_book origin master --squash
 git subtree push --prefix=_book origin gh-pages
 
 

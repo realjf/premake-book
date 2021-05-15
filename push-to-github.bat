87bdd752792ddcc1f2500f83c13c0ee2bf15b7b8
realjf <real_jf@hotmail.com>
2021-05-15 13:37:20 +0800

    updates

--- push-to-github.bat
+++ push-to-github.bat
@@ -7,7 +7,7 @@ git add -A
 git commit -m "updates"
 git push origin master
 
-git subtree pull --prefix=_book origin --squash
+git subtree pull --prefix=_book origin gh-pages --squash
 git subtree push --prefix=_book origin gh-pages
 
 echo "done"

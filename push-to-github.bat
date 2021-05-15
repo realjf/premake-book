dc408fb0a1267ddce611586895660bbf932bd60e
realjf <real_jf@hotmail.com>
2021-05-15 13:13:35 +0800

    updates

--- push-to-github.bat
+++ push-to-github.bat
@@ -7,8 +7,18 @@ git add -A
 git commit -m "updates"
 git push origin master
 
+rmdir /s /Q _book
+
 gitbook build
-git subtree add --prefix=_book origin gh-pages --squash
-git subtree push --prefix=_book origin gh-pages
+git worktree prune
+
+rem checking out gh-pages branch into public
+git worktree add -B gh-pages public origin/gh-pages
+cd _book
+git add --all
+git commit -m "update"
+cd ..
+rem push to repo
+git push origin gh-pages
 
 echo "done"
